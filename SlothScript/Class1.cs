5ea19391211687701fc5a3d8d2ffb79a3ee7f195
inspoy <inspoy@163.com>
2017-09-17 01:52:39 +0800

    创建项目文件

---
+++ SlothScript/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SlothScript
+{
+    public class Class1
+    {
+    }
+}
---
+++ UnitRunner/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SlothScript;
+
+namespace UnitRunner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
